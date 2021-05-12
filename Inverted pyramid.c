35be6c682fba0e9af7c3f2fa84c91c1e5e7ec0b0
Teja chinitha <82213085+Teja-07@users.noreply.github.com>
2021-05-12 16:14:52 +0530

    Add files via upload

---
+++ Inverted pyramid.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int rows,i,j;
+    printf("Enter the rows:");
+    scanf("%d",&rows);
+    for(i=rows;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
