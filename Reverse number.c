fdac4a85d5d822aec293b9fc24ef1f8b44ee0559
Teja chinitha <82213085+Teja-07@users.noreply.github.com>
2021-05-21 13:50:59 +0530

    Add files via upload

---
+++ Reverse number.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,rev=0,remainder;
+    printf("Enter the numbers to be insert:\n");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        remainder=n%10;
+        rev=rev*10+remainder;
+        n/=10;
+        printf("Reversed number=%d",rev);
+    }
+    return 0;
+}
